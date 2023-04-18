ba0809881b49069f7749328aecd24f9b4d58d41d
chenxu <chenxu_2349@qq.com>
2023-04-18 16:04:17 +0800

    v0.2 实现用户上线广播通知功能

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Golang_IM_System
 学习简易Go网络编程，实现一个简单即时通讯系统
+
+## 一、构建基础server  
+1、构建server类型  
+2、创建一个server对象  
+3、启动server服务  
+4、处理链接业务  
+![img.png](images/img2.png)
+
+
+## 二、用户上线功能
+1、创建user类型  
+2、创建user对象，并创建方法监听user对应的channel消息  
+3、改写server：新增OnlineMap和Message属性，在处理客户端上线的Handler中创建并
+添加用户，新增广播消息方法，新增监听广播消息channel的方法，然后启用协程去监听server中
+的Message
+![img.png](images/img1.png)
--- server.go
+++ server.go
@@ -3,18 +3,28 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// 当前在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播的channel
+	Message chan string
 }
 
 // 创建一个server的接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 
 	return server
@@ -23,6 +33,41 @@ func NewServer(ip string, port int) *Server {
 func (this *Server) Handler(conn net.Conn) {
 	// ...当前链接的业务
 	fmt.Println("链接建立成功！")
+
+	// 链接成功后，就创建一个user
+	user := NewUser(conn)
+
+	// 用户上线，将用户添加到OnlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	// 广播当前用户上线信息，那就需要写一个广播方法
+	this.BroadCast(user, "is online...")
+
+	// 当前Handler阻塞
+	select {}
+
+}
+
+// 广播消息方法
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
+// 启用一个协程监听Message消息，一旦有消息就发送给全部在线User
+func (this *Server) MessageListener() {
+	for {
+		msg := <-this.Message
+		// 将msg发送给全部在线用户
+		this.mapLock.Lock()
+		for _, user := range this.OnlineMap {
+			user.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
 }
 
 // 启动服务器的接口
@@ -36,6 +81,9 @@ func (this *Server) Start() {
 
 	// close listen socket
 	defer listener.Close()
+
+	// 启动监听Message的协程
+	go this.MessageListener()
 
 	for {
 		// accept
